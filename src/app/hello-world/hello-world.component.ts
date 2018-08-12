dd63a181c45bbbd2180b7e8f9cb812550e29da22
KAD <kieuanhdung90@gmail.com>
2018-08-12 23:08:18 +0700

    updated

---
+++ src/app/hello-world/hello-world.component.ts
@@ -0,0 +1,17 @@
+// The import statement defines the modules we want to use to write our code.
+// We import Component from the module "@angular/core"
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-world',
+  templateUrl: './hello-world.component.html',
+  styleUrls: ['./hello-world.component.css']
+})
+export class HelloWorldComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
