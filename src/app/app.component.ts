5cce2aad58399219ee6026de4af2f563b4d88612
KAD <kieuanhdung90@gmail.com>
2018-08-21 23:35:16 +0700

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
 
-// Component Metadata: which is used
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 
-// Function class = controller
 export class AppComponent {
   title = 'Angular: Getting Started';
 }
